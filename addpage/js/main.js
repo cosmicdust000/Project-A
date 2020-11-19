b9a8489445fee68ae6a1687a246d93c3f826c198
cosmicdust000 <cosmicdust.anonym000@yandex.ru>
2020-11-19 13:42:25 +0400

    version2.0

--- addpage/js/main.js
+++ addpage/js/main.js
@@ -1,1 +1,12 @@
-"use strict";
+'use strict';
+
+document.forms.formForValues.onsubmit = function() { //При нажатии кнопки в форме, я вызываю функцию, которая берёт данные с полей
+	let eventName = this.eventname.value;
+	let adress = this.adress.value;
+	let date = this.date.value;
+
+	document.querySelector(".eventname").innerHTML = `${eventName}`;
+	document.querySelector(".location").innerHTML = `${adress}`;
+	document.querySelector(".date").innerHTML = `${date}`;
+	return false;
+};
